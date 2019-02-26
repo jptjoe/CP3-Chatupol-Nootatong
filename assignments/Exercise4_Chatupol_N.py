641279fc7fa1f0de6849379ddd9712933acda4c6
jptjoe <jp_ntt@hotmail.com>
2019-02-26 19:31:49 +0700

    Create Exercise4_Chatupol_N.py

---
+++ assignments/Exercise4_Chatupol_N.py
@@ -0,0 +1,9 @@
+FE = 80
+GB = 83.5
+IC = 90.25
+CP = 95.5
+print("--- Your Score ---")
+print("Foundation English :" , FE)
+print("General Business :" , GB)
+print("Introduction to Computer Systems :" , IC)
+print("Computer Programming :" , CP)
